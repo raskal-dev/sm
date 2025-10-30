cdb75073f064c2f70e4954d65a2d4dd640ed4ced
Kalvin RASAMY <kalvinrasamyzo@gmail.com>
2025-10-30 13:21:05 +0300

    feat(user): Ajoute champs pour authentification
    Ajoute les champs 'isActive' et 'passwordHash' à l'entité User.
    
    Ces champs sont nécessaires pour la gestion de l'authentification et du statut d'activation de l'utilisateur.
    - Ajoute le champ `isActive` pour gérer le statut d'activation de l'utilisateur.
    - Ajoute le champ `passwordHash` pour stocker le hachage du mot de passe de l'utilisateur.

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -4,10 +4,12 @@ export class UserEntity {
   public email: string;
   public readonly createdAt: Date;
   public readonly updatedAt: Date;
+  public isActive: boolean;
   public firstName?: string;
   public tel?: string;
   public address?: string;
   public image?: string;
+  public passwordHash?: string;
 
   constructor(data: {
     id: string;
@@ -15,20 +17,24 @@ export class UserEntity {
     email: string;
     createdAt: Date;
     updatedAt: Date;
+    isActive: boolean;
     firstName?: string | null;
     tel?: string | null;
     address?: string | null;
     image?: string | null;
+    passwordHash?: string | null;
   }) {
     this.id = data.id;
     this.lastName = data.lastName;
     this.email = data.email;
     this.createdAt = data.createdAt;
     this.updatedAt = data.updatedAt;
+    this.isActive = data.isActive;
 
     this.firstName = data.firstName ?? undefined;
     this.tel = data.tel ?? undefined;
     this.address = data.address ?? undefined;
     this.image = data.image ?? undefined;
+    this.passwordHash = data.passwordHash ?? undefined;
   }
 }
